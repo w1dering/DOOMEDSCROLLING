585dcfc35a66b33c3eca88ca1a56ceb58b521891
w1dering <ryan.zhang.623@gmail.com>
2025-01-18 21:31:59 -0500

    added user gifs to icon

--- src/components/Users/User/Icon.tsx
+++ src/components/Users/User/Icon.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import bedrotting from "../../../assets/users/bedrotting.gif";
 import deskrotting from "../../../assets/users/deskrotting.gif";
 import floorrotting from "../../../assets/users/floorrotting.gif";
+import carrotting from "../../../assets/users/carrotting.gif";
+import couchrotting from "../../../assets/users/couchrotting.gif";
+import showerrotting from "../../../assets/users/showerrotting.gif";
+import starerotting from "../../../assets/users/starerotting.gif";
+import toiletrotting from "../../../assets/users/floorrotting.gif";
+
 import "./Icon.css";
 
 interface User {
@@ -8,18 +15,33 @@ interface User {
 	name: string;
 }
 
-const userSprites = [bedrotting, deskrotting, floorrotting];
+const userSprites = [
+	bedrotting,
+	deskrotting,
+	floorrotting,
+	carrotting,
+	couchrotting,
+	showerrotting,
+	starerotting,
+	toiletrotting,
+];
 
 interface IconProps {
 	user: User;
 }
 
-
 const Icon = ({ user }: IconProps) => {
-    
+	const [currentUserIndex, setCurrentUserIndex] = useState(-1);
+	if (currentUserIndex == -1) {
+		setCurrentUserIndex(Math.floor(Math.random() * userSprites.length));
+	}
 	return (
 		<div className="icon_container">
-			<img src={userSprites[Math.floor(Math.random() * userSprites.length)]} alt="logo" className="icon" />
+			<img
+				src={userSprites[currentUserIndex]}
+				alt="logo"
+				className="icon"
+			/>
 		</div>
 	);
 };
