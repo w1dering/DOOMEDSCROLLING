3b47c62e0c695b50d47a55598ab0cd6e82648495
JamieXW <jamiewang27@gmail.com>
2025-01-18 20:06:10 -0500

    random gifs

--- src/components/Shorts/ShortsPanel.tsx
+++ src/components/Shorts/ShortsPanel.tsx
@@ -8,7 +8,11 @@ import swordFight from "../../assets/thumbnails/swordFight.gif";
 import tennis from "../../assets/thumbnails/tennis.gif";
 import volcano from "../../assets/thumbnails/volcano.gif";
 import anime from "../../assets/thumbnails/anime.gif";
-
+import car from "../../assets/thumbnails/car.gif";
+import finance from "../../assets/thumbnails/finance.gif";
+import astronomy from "../../assets/thumbnails/astronomy.gif";
+import romance from "../../assets/thumbnails/romance.gif";
+import boardgame from "../../assets/thumbnails/boardgame.gif";
 
 import "./ShortsPanel.css";
 
@@ -17,10 +21,17 @@ const ShortsPanel = () => {
     const [slotsRandom] = useState(["Slot 1", "Slot 2", "Slot 3", "Slot 4",  "Slot 2",
                                 "Slot 2",  "Slot 2",  "Slot 2",  "Slot 2", "Slot 2"])
 
+    const availableGifs = [volcano, anime, car, finance, astronomy, romance, boardgame, food, puppy, singer, swordFight, tennis]; 
+
     const addSlot = () => {
         const newSlot = `Slot ${slotsTrending.length + 1}`;
         setSlotsTrending([...slotsTrending, newSlot]);
     };
+
+    const getRandomGif = () => {
+        const randomIndex = Math.floor(Math.random() * availableGifs.length);
+        return availableGifs[randomIndex];
+    }; 
 
     return (
         <div id="shorts-panel">
@@ -40,7 +51,7 @@ const ShortsPanel = () => {
                 <h1>Random</h1>
                 <div className="slots-wrapper">
                     {slotsRandom.map((slot) => (
-                        <Short content={{img: volcano, text: slot}}/>
+                        <Short content={{img: getRandomGif(), text: slot}}/>
                     ))}
                 </div>
             </div>
